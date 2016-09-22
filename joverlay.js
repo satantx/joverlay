4cf32d5a072f8f213f8052996fc2a8407744d56b
satantx <satantx@gmail.com>
2016-09-22 16:51:45 +0300

    Update joverlay.js

--- joverlay.js
+++ joverlay.js
@@ -6,20 +6,20 @@ $.fn.joverlay = function(options) {
 	return this.each(function(){
 		var joverlay = $(this),
 			 joverlayMain = joverlay.attr('href');
+			 
 		$(joverlayMain).addClass('joverlay-box');
 
 		$(joverlayMain).wrapInner('<div class="joverlay-tb"><div class="joverlay-td"><div class="joverlay-main"></div><div class="joverlay-shadow"></div></div></div>');
 		if (options.close) {
 			$('.joverlay-main', joverlayMain).prepend('<div class="joverlay-close">close</div>');
 		};
+
 		$(joverlay).on('click', function() {
 			$(joverlayMain).show();
 			return false;
 		});
-		$('.joverlay-close', joverlayMain).on('click', function() {
-			$(joverlayMain).hide();
-		});
-		$('.joverlay-shadow').on('click',function() {
+
+		$('.joverlay-close, .joverlay-shadow', joverlayMain).on('click', function() {
 			$(joverlayMain).hide();
 		});
 	});
