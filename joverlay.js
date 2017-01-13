c8f73988fe8ba85eff6e06f948b5bd0571158c91
satantx <satantx@gmail.com>
2017-01-13 16:50:44 +0300

    Update joverlay.js

--- joverlay.js
+++ joverlay.js
@@ -7,6 +7,13 @@ $.fn.joverlay = function(options) {
 	return this.each(function(){
 		var joverlay = $(this),
 			 joverlayMain = joverlay.attr('href');
+
+		var wi = (function(){
+	      var $outer = $('<div>').css({visibility: 'hidden', width: 100, overflow: 'scroll'}).appendTo('body'),
+	          widthWithScroll = $('<div>').css({width: '100%'}).appendTo($outer).outerWidth();
+	      $outer.remove();
+	      return 100 - widthWithScroll;
+	  }());
 			 
 		if (!$(joverlayMain).hasClass('joverlay-box')) {
 			$(joverlayMain).wrapInner('<div class="joverlay-tb"><div class="joverlay-td"><div class="joverlay-main"></div><div class="joverlay-shadow"></div></div></div>').addClass('joverlay-box');
@@ -18,11 +25,13 @@ $.fn.joverlay = function(options) {
 		$(joverlay).on('click', function() {
 			$('.joverlay-box').hide();
 			$(joverlayMain).show();
+			$('body').removeClass('hidden').css({'paddingRight': 0});
 			return false;
 		});
 
 		$('.joverlay-close, .joverlay-shadow', joverlayMain).on('click', function() {
 			$(joverlayMain).hide();
+			$('body').removeClass('hidden').css({'paddingRight': 0});
 		});
 	});
 };
