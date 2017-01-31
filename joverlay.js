afb7a04f3eb1bad2ac7f3dc7794266b4ec5ad27c
satantx <satantx@gmail.com>
2017-01-31 17:49:38 +0300

    Update joverlay.js

--- joverlay.js
+++ joverlay.js
@@ -1,12 +1,14 @@
 $.fn.joverlay = function(options) {
 	var options = $.extend({
 		close: true,
-		closeText: 'Закрыть'
+		closeText: 'Закрыть',
+		closeClass: ''
 	},
 	options);
 	return this.each(function(){
 		var joverlay = $(this),
-			 joverlayMain = joverlay.attr('href');
+			 joverlayMain = joverlay.attr('href'),
+			 closeClassText = ''+options.closeClass+' joverlay-close';
 
 		var wi = (function(){
 	      var $outer = $('<div>').css({visibility: 'hidden', width: 100, overflow: 'scroll'}).appendTo('body'),
@@ -18,14 +20,14 @@ $.fn.joverlay = function(options) {
 		if (!$(joverlayMain).hasClass('joverlay-box')) {
 			$(joverlayMain).wrapInner('<div class="joverlay-tb"><div class="joverlay-td"><div class="joverlay-main"></div><div class="joverlay-shadow"></div></div></div>').addClass('joverlay-box');
 			if (options.close == true) {
-				$('.joverlay-main', joverlayMain).prepend('<div class="joverlay-close">'+options.closeText+'</div>');
+				$('.joverlay-main', joverlayMain).prepend('<div class="'+closeClassText+'">'+options.closeText+'</div>');
 			};
 		}
 
 		$(joverlay).on('click', function() {
 			$('.joverlay-box').hide();
 			$(joverlayMain).show();
-			$('body').removeClass('hidden').css({'paddingRight': 0});
+			$('body').addClass('hidden').css({'paddingRight': wi});
 			return false;
 		});
 
